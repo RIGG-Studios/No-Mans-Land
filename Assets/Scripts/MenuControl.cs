c29116e5c96051ff9fa9aaeb68866910d162a842
ArgOTU <93955974+ArgOTU@users.noreply.github.com>
2023-01-22 23:00:13 -0500

    added pause menu
    Changes:
    -"Play" button now takes you to a submenu where you can choose between Quickplay (not implemented) and the server browser, instead of taking you directly to the server browser
    -Options menu now has proper tabs. There isn't anything in them yet, but I can confirm after testing that the tabs themselves work as intended
    -Added a prompt for the player to input a name before loading the menu; this doesn't actually do anything yet, and is just a placeholder until I implement the config file
    -Updated the MenuControl script to track a few player settings as variables; this is currently just groundwork for when I get the options menu working
    -Added a first draft of the in-game pause menu

--- Assets/Scripts/MenuControl.cs
+++ Assets/Scripts/MenuControl.cs
@@ -6,6 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class MenuControl : MonoBehaviour
 {
+    string name = "";
+    float volumeLevel = 1.0f;
+    float mouseSens = 5.0f;
+
+    void Start(){
+        //loadConfig();
+    }
+
+    public void setName(string newName){
+        name = newName;
+    }
 
     //closes the game (does not work in editor, but should work in an actual build)
     public void ExitButton(){
