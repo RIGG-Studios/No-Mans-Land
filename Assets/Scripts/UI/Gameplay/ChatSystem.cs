28e93b01d14f2d4f710233dc42d8abb4fb2a6bd6
ArgOTU <93955974+ArgOTU@users.noreply.github.com>
2023-03-06 15:05:24 -0500

    updated chat system
    Should now properly take in player input while the chat box is open.

--- Assets/Scripts/UI/Gameplay/ChatSystem.cs
+++ Assets/Scripts/UI/Gameplay/ChatSystem.cs
@@ -1,24 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Fusion;
+using TMPro;
 
 public class ChatSystem : NetworkBehaviour {
-    public MessageBox chatBox;
+    [SerializeField] private MessageBox chatBox;
+    [SerializeField] private GameObject chatUI;
+    [SerializeField] private TMP_InputField chatInput;
 
-    //This method should be called when the player hits Enter while typing
-    public void SendMessage(string username, string message){
-        if(message != ""){
-            RPC_ChatMessage($"<b>{username}:</b> {message}");
+    bool playerIsTyping = false;
+    string messageToSend = "";
+    string name = "Player";
+
+    void Awake(){
+        name = this.GetComponent<Player>().PlayerName.ToString();
+    }
+
+    void Update(){
+        if(playerIsTyping){
+            updateMessage(messageToSend);
         }
     }
 
-    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void updateMessage(string message){
+        foreach(char c in Input.inputString){
+            if(c == '\b'){
+                int length = message.Length;
+                if(message.Length >= 1){
+                    message = message.Substring(0, message.Length - 1);
+                }
+
+            } else if(c == '\n' || c == '\r'){
+                if(message != ""){
+                    SendMessage(name, message);
+                    message = "";
+                }
+            } else {
+                message += c;
+            }
+        }
+    }
+
+    public void SendMessage(string username, string message){
+        RPC_ChatMessage($"<b>{username}:</b> {message}");
+    }
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.All)]
     void RPC_ChatMessage(string message, RpcInfo info = default){
         if(chatBox != null){
             chatBox.OnMessageReceived(message);
         }
     }
 
-
+    public void ProcessInput(NetworkInputData input){
+        if(input.Buttons.IsSet(PlayerButtons.ToggleChat)){
+            if(Object.HasInputAuthority){
+                if(!playerIsTyping){
+                    chatUI.SetActive(true);
+                    playerIsTyping = true;
+                } else {
+                    playerIsTyping = false;
+                }
+            }
+            if(Object.HasStateAuthority){
+                //if chat box is open, freeze player movement. If not, turn movement back on.
+                if(playerIsTyping){
+                    //stop movement; all inputs should now be redirected to the Input Field (chatInput);
+                } else {
+                    //turn movement back on
+                }
+            }
+        }
+    }
 }
