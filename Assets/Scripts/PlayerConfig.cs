0a9acbb0a322a60a081a5b42992ef662558fc509
Bilal <86686062+Bilal-A-G@users.noreply.github.com>
2023-03-31 19:43:14 -0400

    Added editor functionality to all options, but only graphics quality actually does something

--- Assets/Scripts/PlayerConfig.cs
+++ Assets/Scripts/PlayerConfig.cs
@@ -7,12 +7,17 @@ public class PlayerConfig
     public string username = "";
 
     //Graphics settings
+    public int graphicsQuality;
+    public int shadowQuality;
+    public int waterQuality;
+    public int antiAliasingQuality;
+    public int ambientOcclusionQuality;
+    public int volumetrics;
 
     //Audio settings
-    public float volumePercent = 100f;
-
-    //Gameplay settings
-    public float mouseSens = 3f;
+    public float masterVolumePercent;
+    public float effectsVolumePercent;
+    public float musicVolumePercent;
 
     public void SetName(string newName)
     {
@@ -23,30 +28,91 @@ public class PlayerConfig
         PlayerPrefs.SetString("Player Name", username);
     }
 
-    public void SetVolume(float newValue)
+    public void SetMasterVolume(float newValue)
     {
-        volumePercent = newValue;
-        AudioListener.volume = (volumePercent / 100);
+        masterVolumePercent = newValue;
+        AudioListener.volume = (masterVolumePercent / 100);
     }
 
-    public void SetSensitivity(float newValue)
+    public void SetEffectsVolume(float newValue)
     {
-        mouseSens = newValue;
+        effectsVolumePercent = newValue;
+        //Implement effects volume
+    }
+
+    public void SetMusicVolume(float newValue)
+    {
+        musicVolumePercent = newValue;
+        //Implement music volume
+    }
+
+    public void SetGraphicsQuality(int newQuality)
+    {
+        graphicsQuality = newQuality;
+        QualitySettings.SetQualityLevel(newQuality, true);
+    }
+
+    public void SetShadowQuality(int qualityIndex)
+    {
+        shadowQuality = qualityIndex;
+        //Implement shadow quality
+    }
+
+    public void SetWaterQuality(int qualityIndex)
+    {
+        waterQuality = qualityIndex;
+        //Implement water quality
+    }
+
+    public void SetAAQuality(int qualityIndex)
+    {
+        antiAliasingQuality = qualityIndex;
+        //Implement AA quality
+    }
+
+    public void SetAOQuality(int qualityIndex)
+    {
+        ambientOcclusionQuality = qualityIndex;
+        //Implement AO quality
+    }
+
+    public void SetVolumetricQuality(int qualityIndex)
+    {
+        volumetrics = qualityIndex;
+        //Implement volumetrics
     }
 
     //Loads current settings from Unity's PlayerPrefs
     public void LoadSettings()
     {
-        SetVolume(PlayerPrefs.GetFloat("Volume Percentage", 100f));
-        SetSensitivity(PlayerPrefs.GetFloat("Mouse Sensitivity", 3f));
+        SetMasterVolume(PlayerPrefs.GetFloat("Master Volume", 100f));
+        SetEffectsVolume(PlayerPrefs.GetFloat("Effects Volume", 100f));       
+        SetMusicVolume(PlayerPrefs.GetFloat("Music Volume", 100f));
+
         SetName(PlayerPrefs.GetString("Player Name", "Player Name"));
+        SetGraphicsQuality(PlayerPrefs.GetInt("Graphics Quality", 0));
+
+        SetVolumetricQuality(PlayerPrefs.GetInt("Volumetrics", 0));
+        SetShadowQuality(PlayerPrefs.GetInt("Shadow Quality", 0));
+        SetWaterQuality(PlayerPrefs.GetInt("Water Quality", 0));
+        SetAAQuality(PlayerPrefs.GetInt("AA Quality", 0));
+        SetAOQuality(PlayerPrefs.GetInt("AO Quality", 0));
     }
 
     //Saves current settings to Unity's PlayerPrefs
     public void SaveSettings()
     {
-        PlayerPrefs.SetFloat("Volume Percentage", volumePercent);
-        PlayerPrefs.SetFloat("Mouse Sensitivity", mouseSens);
+        PlayerPrefs.SetFloat("Master Volume", masterVolumePercent);
+        PlayerPrefs.SetFloat("Effects Volume", effectsVolumePercent);
+        PlayerPrefs.SetFloat("Music Volume", musicVolumePercent);
+
+        PlayerPrefs.SetInt("Graphics Quality", graphicsQuality);
+        PlayerPrefs.SetInt("Shadow Quality", shadowQuality);
+
+        PlayerPrefs.SetInt("Water Quality", waterQuality);
+        PlayerPrefs.SetInt("AA Quality", antiAliasingQuality);
+        PlayerPrefs.SetInt("AO Quality", ambientOcclusionQuality);
+        PlayerPrefs.SetInt("Volumetrics", volumetrics);
     }
 
     //Resets all settings to default
--- Assets/Scripts/UI/Menu/MenuController.cs
+++ Assets/Scripts/UI/Menu/MenuController.cs
@@ -12,23 +12,52 @@ public class MenuController : MonoBehaviour
     [SerializeField] private GameObject connectionScreen;
 
     [SerializeField] private TextMeshProUGUI welcomeText;
-    [SerializeField] private Slider volumeSlider;
-    [SerializeField] private TextMeshProUGUI volumeText;
     [SerializeField] private TMP_InputField nameText;
 
+    [Header("Audio")]
+    [SerializeField] private Slider masterVolumeSlider;
+    [SerializeField] private TextMeshProUGUI masterVolumeText;
+    [SerializeField] private Slider effectsVolumeSlider;
+    [SerializeField] private TextMeshProUGUI effectsVolumeText;
+    [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private TextMeshProUGUI musicVolumeText;
+
+    [Header("Graphics")]
+    [SerializeField] private TMP_Dropdown graphicsQualityDropdown;
+    [SerializeField] private TMP_Dropdown shadowQualityDropdown;
+    [SerializeField] private TMP_Dropdown waterQualityDropdown;
+    [SerializeField] private TMP_Dropdown AntiAliasingDropdown;
+    [SerializeField] private TMP_Dropdown AmbientOcclusionDropdown;
+    [SerializeField] private TMP_Dropdown VolumetricsDropdown;
 
     [SerializeField] private CanvasFader canvasFader;
 
     private PlayerConfig _config;
+    private bool starting = true;
 
     private void Start()
     {
         _config = new PlayerConfig();
         _config.LoadSettings();
         
-        volumeText.text = _config.volumePercent + "%";
-        volumeSlider.value = _config.volumePercent;
+        masterVolumeText.text = _config.masterVolumePercent + "%";
+        masterVolumeSlider.value = _config.masterVolumePercent;
+
+        effectsVolumeText.text = _config.effectsVolumePercent + "%";
+        effectsVolumeSlider.value = _config.effectsVolumePercent;
+
+        musicVolumeText.text = _config.musicVolumePercent + "%";
+        musicVolumeSlider.value = _config.musicVolumePercent;
+
         nameText.text = _config.username;
+        graphicsQualityDropdown.value = _config.graphicsQuality;
+        shadowQualityDropdown.value = _config.shadowQuality;
+        waterQualityDropdown.value = _config.waterQuality;
+        AntiAliasingDropdown.value = _config.antiAliasingQuality;
+        AmbientOcclusionDropdown.value = _config.ambientOcclusionQuality;
+        VolumetricsDropdown.value = _config.volumetrics;
+
+        starting = false;
     }
 
     private void PromptName()
@@ -44,16 +73,67 @@ public class MenuController : MonoBehaviour
         Invoke(nameof(ShowMainMenu), 1.0f);
     }
 
-    public void SetVolume(float value)
+    public void SetMasterVolume(float value)
     {
-        volumeText.text = value + "%";
-        _config.SetVolume(value);
+        masterVolumeText.text = value + "%";
+        _config.SetMasterVolume(value);
     }
 
-    public void SetName(string name)
+    public void SetEffectsVolume(float value)
     {
-        _config.SetName(name);
+        effectsVolumeText.text = value + "%";
+        _config.SetEffectsVolume(value);
     }
+
+    public void SetMusicVolume(float value)
+    {
+        musicVolumeText.text = value + "%";
+        _config.SetMusicVolume(value);
+    }
+
+    public void SetGraphicsQuality(int qualityIndex)
+    {
+        if(!starting)
+        {
+            shadowQualityDropdown.value = qualityIndex;
+            waterQualityDropdown.value = qualityIndex;
+            AntiAliasingDropdown.value = qualityIndex;
+        }
+
+        if(qualityIndex == 0)
+        {
+            if(!starting)
+            {
+                AmbientOcclusionDropdown.value = 0;
+                VolumetricsDropdown.value = 0;
+            }
+        }
+        else 
+        {
+            if(!starting)
+            {
+                AmbientOcclusionDropdown.value = 1;
+            }
+        }
+
+        if(qualityIndex == 2)
+        {
+            if(!starting)
+            {
+                VolumetricsDropdown.value = 3;
+            }
+        }
+
+        _config.SetGraphicsQuality(qualityIndex);
+    }
+
+    public void SetShadowQuality(int qualityIndex) => _config.SetShadowQuality(qualityIndex);
+    public void SetWaterQuality(int qualityIndex) => _config.SetWaterQuality(qualityIndex);
+    public void SetAAQuality(int qualityIndex) => _config.SetAAQuality(qualityIndex);
+    public void SetAOQuality(int qualityIndex) => _config.SetAOQuality(qualityIndex);
+    public void SetVolumetricQuality(int qualityIndex) => _config.SetVolumetricQuality(qualityIndex);
+
+    public void SetName(string name) => _config.SetName(name);
 
     public void ApplySettings() => _config.SaveSettings(); 
     public void ResetSettings() => _config.ResetSettings();
